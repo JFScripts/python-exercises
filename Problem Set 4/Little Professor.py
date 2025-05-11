7d035fc5352ddde8b74513573223d75df70a893e
João Francisco <joao.bordini42@gmail.com>
2025-05-11 14:34:28 -0300

    Exercise FInished

--- Problem Set 4/Little Professor.py
+++ Problem Set 4/Little Professor.py
@@ -1,8 +1,26 @@
 import random
 
 def main():
-    nivel = get_level("Digite o nivel desejado (1;2;3)\n")
-    generate_problems(nivel)
+    # Quantidade de problemas gerados
+    desafios = 10
+    vida = 3
+    pontos = 0
+    # Pegando o nivel
+    while vida > 0:
+        nivel = get_level("Digite o nivel desejado (1;2;3)\n")
+        print(f"Vida: {vida}\nPontos: {pontos}\n")
+        # Calculando a dificuldade e retornando para um lista
+        dificuldade = calculate_dificulty(nivel)
+
+        # Gerando dois Valores com o intervalo da dificuldade
+        x = generate_numbers(dificuldade[1], dificuldade[0], desafios) #Max - Min
+        y = generate_numbers(dificuldade[1], dificuldade[0], desafios) #Max - Min
+
+        # Calcular as soluções para cada soma
+        pontos_ganho, vida = answers(x, y, desafios, vida)
+        pontos += pontos_ganho
+    print(f"Fim de jogo\nPontuação total: {pontos}")
+
 
 def get_level(Prompt):
     while True:
@@ -17,21 +35,40 @@ def get_level(Prompt):
                 print("Escolha um nivel entre 1;2;3")
 
 
-def generate_problems(dificuldade):
-    dificuldade_max = (10 ** dificuldade)-1
-    dificuldade_min = 10 ** (dificuldade - 1)
-    x, y, s = [], [], []
-    if dificuldade == 1:
-        dificuldade_min = 0
+def calculate_dificulty(escolha):
+    dificuldade = [9, 0]    #Padrão
+    dificuldade[0] = (10 ** escolha) - 1 #Maximo
+    dificuldade[1] = 10 ** (escolha - 1) #Minimo
+    if escolha == 1:
+        dificuldade[1] = 0
+    return dificuldade
 
-    for i in range(10):
-        x.append(random.randrange(dificuldade_min, dificuldade_max))
-        y.append(random.randrange(dificuldade_min, dificuldade_max))
-        s.append(x[i] + y[i])
-        print(f"{x[i]} + {y[i]} = {s[i]}")
+def generate_numbers(dificuldade_min, dificuldade_max, size):
+    n = []
+    for i in range(size):
+        n.append(random.randrange(dificuldade_min, dificuldade_max))
 
-    ##print(f"Max {dificuldade_max}")
-    ##print(f"Min {dificuldade_min}")
+    return n
+
+def answers(n1, n2,size, lives):
+    curDesafio = 0
+    while lives > 0:
+        if curDesafio == size:
+            print(f"Parabens você ganhou esse nivel\nVoce ganhou {curDesafio} pontos")
+            return curDesafio, lives
+        print(f"{(curDesafio)+1:02d}) ", end="")
+        try:
+            tentativa = int(input(f"{n1[curDesafio]} + {n2[curDesafio]} = "))
+        except ValueError:
+            lives -= 1
+            print(f"Resposta Errada\nVida:{lives}")
+        else:
+            if tentativa == (n1[curDesafio] + n2[curDesafio]):
+                curDesafio += 1
+            else:
+                lives -= 1
+                print(f"Resposta Errada\nVida: {lives}")
+    return curDesafio, lives
 
 
 
